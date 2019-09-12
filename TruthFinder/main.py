dd866e08092f1aefffcc01cf882eac761195c8c7
Knt44kw <a_voandelevire.i@jcom.zaq.ne.jp>
2019-09-12 19:36:46 +0900

    add numerical object & refactor script

--- TruthFinder/main.py
+++ TruthFinder/main.py
@@ -1,13 +1,29 @@
-import csv
 import pandas as pd
 import numpy as np
 from numpy.linalg import norm
 from sklearn.feature_extraction.text import TfidfVectorizer
-
 from truthdiscovery import TruthFinder
 
-
-dataframe = pd.DataFrame([
+def execute_truthfinder(df):
+    vectorizer = TfidfVectorizer(min_df=1)
+    vectorizer.fit(df["fact"]) # 事実を文章とみなして，ベクトル化している
+    
+    def similarity(w1, w2) -> float:
+        V = vectorizer.transform([w1, w2]) # 単語をスパース(0ばっかり)行列に変換
+        v1, v2 = np.asarray(V.todense()) # 普通の行列に変換
+        return np.dot(v1, v2) / (norm(v1) * norm(v2)) # コサイン類似度(単語の類似度)を求める
+        
+    def implication(f1, f2) -> float:
+        return similarity(f1.lower(), f2.lower()) # 事実を文章とみなしたとき，単語の類似度を求める．
+        
+    finder = TruthFinder(implication, dampening_factor=0.8, influence_related=0.6)
+    
+    print("\nInital state \n{}".format(df))
+    df = finder.train(df)    
+    print("\nEstimation result \n{}".format(df))
+    
+if __name__ =="__main__":
+    dataframe = pd.DataFrame([
         ["a", "Einstein", "Special relativity"],
         ["a", "Newton", "Universal gravitation"],
         ["b", "Albert Einstein", "Special relativity"],
@@ -17,28 +33,62 @@ dataframe = pd.DataFrame([
         ["c", "Einstein", "Heliocentrism"]
     ],
     columns=["website", "fact", "object"]
-)
+    )    
 
+    # objectを数値にする a,bが正しい cは誤り 
+    dataframe_numerical = pd.DataFrame([
+        ["a", "Fuji", 3776],
+        ["a", "K2", 8611],
+        ["b", "Mt.Fuji", 3776],
+        ["b", "Mt.Everest", 8848],
+        ["c", "Karakorum No.2", 3776],
+        ["c", "Everest", 8611],
+        ["c", "Fuji", 8848]
+    ],
+    columns=["website", "fact", "object"]
+    )
 
-vectorizer = TfidfVectorizer(min_df=1)
-vectorizer.fit(dataframe["fact"])
+    execute_truthfinder(dataframe)
+    execute_truthfinder(dataframe_numerical)
 
+"""
+Inital state 
+  website             fact                 object
+0       a         Einstein     Special relativity
+1       a           Newton  Universal gravitation
+2       b  Albert Einstein     Special relativity
+3       b  Galileo Galilei          Heliocentrism
+4       c           Newton     Special relativity
+5       c          Galilei  Universal gravitation
+6       c         Einstein          Heliocentrism
 
-def similarity(w1, w2):
-    V = vectorizer.transform([w1, w2])
-    v1, v2 = np.asarray(V.todense())
-    return np.dot(v1, v2) / (norm(v1) * norm(v2))
+Estimation result 
+  website             fact                 object  trustworthiness  fact_confidence
+0       a         Einstein     Special relativity         0.862090         0.894279
+1       a           Newton  Universal gravitation         0.862090         0.829901
+2       b  Albert Einstein     Special relativity         0.862090         0.894279
+3       b  Galileo Galilei          Heliocentrism         0.862090         0.829901
+4       c           Newton     Special relativity         0.754878         0.754878
+5       c          Galilei  Universal gravitation         0.754878         0.754878
+6       c         Einstein          Heliocentrism         0.754878         0.754878
 
+Inital state 
+  website            fact  object
+0       a            Fuji    3776
+1       a              K2    8611
+2       b         Mt.Fuji    3776
+3       b      Mt.Everest    8848
+4       c  Karakorum No.2    3776
+5       c         Everest    8611
+6       c            Fuji    8848
 
-def implication(f1, f2):
-    return similarity(f1.lower(), f2.lower())
-
-
-finder = TruthFinder(implication, dampening_factor=0.8, influence_related=0.6)
-
-print("Inital state")
-print(dataframe)
-dataframe = finder.train(dataframe)
-
-print("Estimation result")
-print(dataframe)
+Estimation result 
+  website            fact  object  trustworthiness  fact_confidence
+0       a            Fuji    3776         0.876433         0.910919
+1       a              K2    8611         0.876433         0.841946
+2       b         Mt.Fuji    3776         0.876433         0.910919
+3       b      Mt.Everest    8848         0.876433         0.841946
+4       c  Karakorum No.2    3776         0.754878         0.754878
+5       c         Everest    8611         0.754878         0.754878
+6       c            Fuji    8848         0.754878         0.754878
+"""
--- TruthFinder/truthdiscovery.py
+++ TruthFinder/truthdiscovery.py
@@ -1,14 +1,13 @@
 import numpy as np
 from numpy.linalg import norm
-import math
-# http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.130.6108&rep=rep1&type=pdf
-
+import pandas as pd
 import warnings
 warnings.filterwarnings("error")
+# http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.130.6108&rep=rep1&type=pdf
 
 
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 class TruthFinder(object):
@@ -30,7 +29,7 @@ class TruthFinder(object):
         self.dampening_factor = dampening_factor
         self.influence_related = influence_related
 
-    def adjust_confidence(self, df):
+    def adjust_confidence(self, df) -> pd.DataFrame:
         """Eq. 6"""
 
         update = {}
@@ -42,57 +41,59 @@ class TruthFinder(object):
                 if f1 == f2:
                     continue
                 s += row2["fact_confidence"] * self.implication(f2, f1)
-            update[i] = self.influence_related * s + row1["fact_confidence"]
+            update[i] = row1["fact_confidence"] + self.influence_related * s 
 
         for i, row1 in df.iterrows():
             df.at[i, "fact_confidence"] = update[i]
 
         return df
 
-    def calculate_confidence(self, df):
-        trustworthiness_score = lambda x: -math.log(1-x)  # Eq. 3
-
+    def calculate_confidence(self, df) -> pd.DataFrame:
+        trustworthiness_score = lambda x: -np.log(1-x)  # Eq. 3
         """Calculate confidence for each fact"""
         for i, row in df.iterrows():
             # Eq. 5
             # trustworthiness of corresponding websites `W(f)`
-            ts = df.loc[df["fact"] == row["fact"], "trustworthiness"]
-            v = sum(trustworthiness_score(t) for t in ts)
-            df.at[i, "fact_confidence"] = v
+            ts = df.loc[df["fact"] == row["fact"], "trustworthiness"] # 情報源の順番とtrustworthinessを格納
+            v = sum(trustworthiness_score(t) for t in ts) # trustworthinessを代入することで，ある情報源そのものの信憑性を求める(trustwothiness_score)
+            df.at[i, "fact_confidence"] = v # 各情報源が伝える情報の信憑性(fact_confidence)を代入(a 0.8 a 0.9 … b 0.76 b 0.55 b 0.90，…)
         return df
 
-    def compute_fact_confidence(self, df):
+    def compute_fact_confidence(self, df) -> pd.DataFrame:
+        """
+        情報源が独立ではないことを考慮に入れた処理 論文3.1.3節
+        """
         f = lambda x: sigmoid(self.dampening_factor * x)
         for i, row in df.iterrows():
             df.at[i, "fact_confidence"] = f(row["fact_confidence"])
         return df
 
-    def update_fact_confidence(self, df):
+    def update_fact_confidence(self, df) -> pd.DataFrame:
         for object_ in df["object"].unique():
             indices = df["object"] == object_
             d = df.loc[indices]
-            d = self.calculate_confidence(d)
-            d = self.adjust_confidence(d)
-            df.loc[indices] = self.compute_fact_confidence(d)
+            d = self.calculate_confidence(d) # 情報源が1つの場合を想定し，fact_confidenceに補正をかけないで求める
+            d = self.adjust_confidence(d) # 情報源が複数の場合(=情報源間に相関性が出ること)を考慮して，fact_confidenceを求める
+            df.loc[indices] = self.compute_fact_confidence(d) # 0〜1に収まるように シグモイド関数を適用し，これを実際のfact_confidenceとする．
         return df
 
-    def update_website_trustworthiness(self, df):
+    def update_website_trustworthiness(self, df) -> pd.DataFrame:
         for website in df["website"].unique():
-            indices = df["website"] == website
-            cs = df.loc[indices, "fact_confidence"]
-            df.loc[indices, "trustworthiness"] = sum(cs) / len(cs)
+            indices = df["website"] == website # 情報源を選ぶ (情報源a，情報源b，情報源c)など
+            cs = df.loc[indices, "fact_confidence"] # 各情報源が伝えた事実に対するfact_condidenceの値を代入
+            df.loc[indices, "trustworthiness"] = sum(cs) / len(cs) # 各情報源のfact_condidenceの合計を各情報源によって伝えられた事実の数で割る = trustworthinessを求める
         return df
-
-    def iteration(self, df):
+    
+    def iteration(self, df) -> pd.DataFrame:
         df = self.update_fact_confidence(df)
         df = self.update_website_trustworthiness(df)
         return df
 
-    def stop_condition(self, t1, t2, threshold):
+    def stop_condition(self, t1, t2, threshold) -> bool:
         return norm(t2-t1) < threshold
 
     def train(self, dataframe, max_iterations=200,
-              threshold=1e-6, initial_trustworthiness=0.9):
+              threshold=1e-6, initial_trustworthiness=0.9) -> pd.DataFrame:
         dataframe["trustworthiness"] =\
                 np.ones(len(dataframe.index)) * initial_trustworthiness
         dataframe["fact_confidence"] = np.zeros(len(dataframe.index))
